c4dfbbf809d3aafbd6c5dea44cf91bff55c90e02
Brock Allen <brockallen@gmail.com>
2014-07-22 11:28:24 -0400

    initial code

---
+++ default.ps1
@@ -0,0 +1,63 @@
+properties {
+	$base_directory = Resolve-Path . 
+	$src_directory = "$base_directory\source"
+	$dist_directory = "$base_directory\distribution"
+	$sln_file = "$src_directory\Thinktecture.IdentityServer.v3.AspNetIdentity.sln"
+	$target_config = "Release"
+	$framework_version = "v4.5"
+	$nuget_path = "$src_directory\.nuget\nuget.exe"
+	
+	$buildNumber = 0;
+	$version = "1.0.0.0"
+	$preRelease = $null
+}
+
+task default -depends Clean, CreateNuGetPackage
+
+task Clean {
+	rmdir $dist_directory -ea SilentlyContinue -recurse
+	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /t:Clean }
+}
+
+task Compile -depends UpdateVersion {
+	exec { msbuild /nologo /verbosity:q $sln_file /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.5 }
+
+	$versionAssemblyInfoFile = "$src_directory/VersionAssemblyInfo.cs"
+	rm $versionAssemblyInfoFile
+}
+
+task UpdateVersion {
+	$vSplit = $version.Split('.')
+	if($vSplit.Length -ne 4)
+	{
+		throw "Version number is invalid. Must be in the form of 0.0.0.0"
+	}
+	$major = $vSplit[0]
+	$minor = $vSplit[1]
+	$patch = $vSplit[2]
+	$assemblyFileVersion =  "$major.$minor.$patch.$buildNumber"
+	$assemblyVersion = "$major.$minor.0.0"
+	$versionAssemblyInfoFile = "$src_directory/VersionAssemblyInfo.cs"
+	"using System.Reflection;" > $versionAssemblyInfoFile
+	"" >> $versionAssemblyInfoFile
+	"[assembly: AssemblyVersion(""$assemblyVersion"")]" >> $versionAssemblyInfoFile
+	"[assembly: AssemblyFileVersion(""$assemblyFileVersion"")]" >> $versionAssemblyInfoFile
+}
+
+task CreateNuGetPackage -depends Compile {
+	$vSplit = $version.Split('.')
+	if($vSplit.Length -ne 4)
+	{
+		throw "Version number is invalid. Must be in the form of 0.0.0.0"
+	}
+	$major = $vSplit[0]
+	$minor = $vSplit[1]
+	$patch = $vSplit[2]
+	$packageVersion =  "$major.$minor.$patch"
+	if($preRelease){
+		$packageVersion = "$packageVersion-$preRelease" 
+	}
+
+	md $dist_directory
+	exec { . $nuget_path pack $src_directory\AspNetIdentityUserService\AspNetIdentityUserService.csproj -o $dist_directory -version $packageVersion }
+}
