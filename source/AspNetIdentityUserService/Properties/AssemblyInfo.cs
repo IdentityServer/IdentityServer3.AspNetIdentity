1f65b651c415f4ab3abe58f114a835632dceb4d8
Brock Allen <brockallen@gmail.com>
2015-04-07 09:23:17 -0400

    update to v2 models; branding changes

--- source/AspNetIdentityUserService/Properties/AssemblyInfo.cs
+++ source/AspNetIdentityUserService/Properties/AssemblyInfo.cs
@@ -20,12 +20,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AspNetIdentityUserService")]
+[assembly: AssemblyTitle("IdentityServer3.AspNetIdentity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AspNetIdentityUserService")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("IdentityServer3.AspNetIdentity")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
